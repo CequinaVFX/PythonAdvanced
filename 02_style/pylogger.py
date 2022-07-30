fb2448e20cbf6f2f67414d89e86d95422cf29ae0
Cequina <lucianocequinel@gmail.com>
2022-07-30 17:29:52 +0100

    Update pylogger.py

--- 02_style/pylogger.py
+++ 02_style/pylogger.py
@@ -12,20 +12,33 @@ def findCaller(self):
     Find the stack frame of the caller so that we can note the source
     file name, line number and function name.
     """
+
     f = currentframe()
+
     #On some versions of IronPython, currentframe() returns None if
     #IronPython isn't run with -X:Frames.
     if f is not None:
         f = f.f_back
-    rv = "(unknown file)", 0, "(unknown function)"
+
+    rv = "(unknown file)",
+         0,
+         "(unknown function)"
+
     while hasattr(f, "f_code"):
+
         co = f.f_code
         filename = os.path.normcase(co.co_filename)
+
         if filename == _srcfile:
             f = f.f_back
             continue
-        rv = (co.co_filename, f.f_lineno, co.co_name)
+
+        rv = (co.co_filename,
+              f.f_lineno,
+              co.co_name)
+
         break
+
     return rv
 
 # How can we make this code better?
