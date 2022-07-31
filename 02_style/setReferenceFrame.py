5166567d8452d5806a31ba3bcb19d0237cb754af
Cequina <lucianocequinel@gmail.com>
2022-07-31 15:12:00 +0100

    FIX: better code and free of errors

--- 02_style/setReferenceFrame.py
+++ 02_style/setReferenceFrame.py
@@ -1,10 +1,11 @@
 #******************************************************
-# content: simple script to quickly update reference frame on a selected node
+# content: simple script to quickly update reference frame on a selected list of nodes
+# if the option to use current frame, or to write a specific one
 #
-# version: 2.1.0
+# version: 2.2.0
 # date: July 31 2022
 #
-# how to: 
+# how to: getFrameUI()
 # dependencies: nuke
 # todos: --//--
 #
@@ -20,68 +21,70 @@ import nuke
 SUPPORTED_NODES = ['Tracker4', 'FrameHold', 'Roto', 'RotoPaint', 'Transform', 'CornerPin2D', 'VectorDistort']
 SUPPORTED_KNOBS = ['reference_frame', 'referenceFrame', 'first_frame', 'ref_frame']
 
-def changeFrame(newFrame, selNodes):
+
+def changeFrame(newFrame, node):
    
-    for node in selNodes:
-
-        #if node.Class() in SUPPORTED_NODES:
-            
-        for knob in node.knobs():
-            if knob in SUPPORTED_KNOBS:
-                node[knob].setValue(newFrame)
-                print ('{}.{} got the value: {}'.format (node.name(), (knob), newFrame))
-
-            else:
-                print ("{} doesn't have a valid knob:".format (node.name()))
+    for knob in node.knobs():
+        if knob in SUPPORTED_KNOBS:
+            node[knob].setValue(int(newFrame))
+            print ('{}.{} got the value: {}'.format (node.name(), (knob), newFrame))
 
 
 def getCurrentFrame():
-    
+    '''
+    use the current frame from current viewer
+    '''
+    newFrame = nuke.frame()
     selNodes = nuke.selectedNodes()
 
-    validNodes = []
+    if len (selNodes) > 0:
+        for node in selNodes:
+            if node.Class() in SUPPORTED_NODES:
+                changeFrame(newFrame, node)
 
-    for node in selNodes:
-        if node.Class() in SUPPORTED_NODES:
-            print ('adding node {}'.format (node.name()))
-            validNodes.append(node)
-
-    if len (validNodes) == 0:
-        nuke.message('Select something!')
+    else:
+        nuke.message('Select some valid node\n{}!'.format (SUPPORTED_NODES))
         return
-
-    newFrame = nuke.frame()
-
-    changeFrame(newFrame, selNodes)
 
 
 def getFrameUI():
+    '''
+    get a specific frame from user
+    '''
 
     selNodes = nuke.selectedNodes()
-
-    if selNodes not in SUPPORTED_NODES:
 
     validNodes = []
 
     for node in selNodes:
         if node.Class() in SUPPORTED_NODES:
-            print ('adding node {}'.format (node.name()))
             validNodes.append(node)
 
-    if len (validNodes) == 0:
-        nuke.message('Select something!')
+    if len (validNodes) > 0:
+
+        newFrame = (nuke.getInput('frame' '%s' % nuke.frame()))
+
+        if newFrame.isdigit():
+            for node in validNodes:
+                changeFrame(newFrame, node)
+        else:
+            nuke.message('You must write an integer value! (e.g 1055)')
+            getFrameUI()
+
+    else:
+        nuke.message('Select some valid node\n{}!'.format (SUPPORTED_NODES))
         return
 
-    newFrame = int(nuke.getInput('frame'))
-
-    changeFrame(newFrame, selNodes)
-
-
-getCurrentFrame()
-#getFrameUI()
 #################################
 #Add to Nodes menu
-#toolbar = nuke.menu('Nodes')
-#cqnTools = toolbar.addMenu('CQNTools', 'Modify.png')
-#cqnTools.addCommand('Set to this frame', 'setRefFrame.getCurrrentFrame()')
-#cqnTools.addCommand('Set to specific frame', 'setRefFrame.getFrameUI()')
+toolbar = nuke.menu('Nodes')
+cqnTools = toolbar.addMenu('CQNTools', 'Modify.png')
+cqnTools.addCommand('Set to this frame', 'setReferenceFrame.getCurrentFrame()')
+cqnTools.addCommand('Set to specific frame', 'setReferenceFrame.getFrameUI()')
+
+
+##########################
+# main function
+if __name__ == '__main__':
+    getFrameUI()
+
